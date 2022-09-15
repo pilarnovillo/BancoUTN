8c84358c3e97446d0d328f2013bc06ae384130c2
pilarnovillo <pilar.novillosaravia@gmail.com>
2022-09-15 15:07:57 +0200

    Avances practica

--- app/src/main/java/ar/NovilloSaravia/banco_utn/MainActivity.java
+++ app/src/main/java/ar/NovilloSaravia/banco_utn/MainActivity.java
@@ -2,7 +2,12 @@ package ar.NovilloSaravia.banco_utn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Getting the instance of Spinner and setting the list from resources
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.Monedas,
+                android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter2);
+
+        //Getting button
+        Button boton = findViewById(R.id.button);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SimularPlazoFijo.class));
+            }
+        });
     }
+
+
 }
+
---
+++ app/src/main/java/ar/NovilloSaravia/banco_utn/SimularPlazoFijo.java
@@ -0,0 +1,14 @@
+package ar.NovilloSaravia.banco_utn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SimularPlazoFijo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simular_plazo_fijo);
+    }
+}
