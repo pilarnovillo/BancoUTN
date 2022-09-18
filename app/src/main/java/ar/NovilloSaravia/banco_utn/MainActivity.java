7a1878df52af648da1c3535a12305d72b23f5f3a
pilarnovillo <pilar.novillosaravia@gmail.com>
2022-09-18 15:20:55 +0200

    Avances practica

--- app/src/main/java/ar/NovilloSaravia/banco_utn/MainActivity.java
+++ app/src/main/java/ar/NovilloSaravia/banco_utn/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -37,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
                                 SimularPlazoFijo.class);
 //                i.putExtra("primerLetra"
 //                        ,edtLetra.getText().toString());
-                startActivityForResult(i,1); //TODO do we still use it like this?
+                startActivityForResult(i, 1); //TODO do we still use it like this?
 //                startActivity(new Intent(MainActivity.this, SimularPlazoFijo.class));
             }
         });
@@ -53,6 +56,32 @@ public class MainActivity extends AppCompatActivity {
                 botonConstruir.setEnabled(true);
                 Double capital = data.getExtras().getDouble("capital");
                 Integer dias = data.getExtras().getInt("dias");
+
+                botonConstruir.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                        EditText editTextTextPersonName = findViewById(R.id.editTextTextPersonName);
+                        EditText editTextTextApellido = findViewById(R.id.editTextTextApellido);
+
+                        builder.setTitle("Felicitaciones " + editTextTextPersonName.getText().toString()+" "+editTextTextApellido.getText().toString());
+                        builder.setMessage("Tu plazo fijo de "+capital.toString()+" pesos por "+dias.toString()+" dias ha sido construido!");
+                        builder.setPositiveButton("piola!", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // START THE GAME!
+                            }
+                        });
+//                        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+//                            public void onClick(DialogInterface dialog, int id) {
+//                                // User cancelled the dialog
+//                            }
+//                        }
+//                        );
+                        builder.show();
+//        // Create the AlertDialog object and return it
+//        return builder.create();
+                    }
+                });
 //                datos.setText("SELECCIONO <" + producto + "> Cantidad:"+cantidad+" unidades ");
             }
         }
