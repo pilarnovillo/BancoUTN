76d8cb7bc19a6370b1476f020c0f57e6a7db068b
pilarnovillo <pilar.novillosaravia@gmail.com>
2022-09-20 16:54:23 +0200

    use binding

--- app/src/main/java/ar/NovilloSaravia/banco_utn/MainActivity.java
+++ app/src/main/java/ar/NovilloSaravia/banco_utn/MainActivity.java
@@ -10,38 +10,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
+import ar.NovilloSaravia.banco_utn.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
         //Getting the instance of Spinner and setting the list from resources
-        Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
                 R.array.Monedas,
                 android.R.layout.simple_spinner_item);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter2);
+        binding.spinner.setAdapter(adapter2);
 
         //Getting button
-        Button boton = findViewById(R.id.button);
-        boton.setOnClickListener(new View.OnClickListener() {
+        binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i =
                         new Intent(MainActivity.this,
                                 SimularPlazoFijo.class);
-//                i.putExtra("primerLetra"
-//                        ,edtLetra.getText().toString());
                 startActivityForResult(i, 1); //TODO do we still use it like this?
-//                startActivity(new Intent(MainActivity.this, SimularPlazoFijo.class));
             }
         });
     }
@@ -52,37 +49,25 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == Activity.RESULT_OK) {
             if (requestCode == 1) {
-                Button botonConstruir = findViewById(R.id.botonConstituir);
-                botonConstruir.setEnabled(true);
+                binding.botonConstituir.setEnabled(true);
                 Double capital = data.getExtras().getDouble("capital");
                 Integer dias = data.getExtras().getInt("dias");
 
-                botonConstruir.setOnClickListener(new View.OnClickListener() {
+                binding.botonConstituir.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
-                        EditText editTextTextPersonName = findViewById(R.id.editTextTextPersonName);
-                        EditText editTextTextApellido = findViewById(R.id.editTextTextApellido);
 
-                        builder.setTitle("Felicitaciones " + editTextTextPersonName.getText().toString()+" "+editTextTextApellido.getText().toString());
+                        builder.setTitle("Felicitaciones " + binding.editTextTextPersonName.getText().toString()+" "+binding.editTextTextApellido.getText().toString());
                         builder.setMessage("Tu plazo fijo de "+capital.toString()+" pesos por "+dias.toString()+" dias ha sido construido!");
                         builder.setPositiveButton("piola!", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
                                 // START THE GAME!
                             }
                         });
-//                        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-//                            public void onClick(DialogInterface dialog, int id) {
-//                                // User cancelled the dialog
-//                            }
-//                        }
-//                        );
                         builder.show();
-//        // Create the AlertDialog object and return it
-//        return builder.create();
                     }
                 });
-//                datos.setText("SELECCIONO <" + producto + "> Cantidad:"+cantidad+" unidades ");
             }
         }
     }
--- app/src/main/java/ar/NovilloSaravia/banco_utn/SimularPlazoFijo.java
+++ app/src/main/java/ar/NovilloSaravia/banco_utn/SimularPlazoFijo.java
@@ -8,20 +8,23 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.SeekBar;
-import android.widget.TextView;
+
+import ar.NovilloSaravia.banco_utn.databinding.ActivitySimularPlazoFijoBinding;
 
 public class SimularPlazoFijo extends AppCompatActivity {
+
+    private ActivitySimularPlazoFijoBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_simular_plazo_fijo);
 
-        EditText textoCapital = findViewById(R.id.textoCapital);
-        textoCapital.addTextChangedListener(new TextWatcher() { //TODO ask about this implementation
+        binding = ActivitySimularPlazoFijoBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.textoCapital.addTextChangedListener(new TextWatcher() { //TODO ask about this implementation
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             }
@@ -36,8 +39,7 @@ public class SimularPlazoFijo extends AppCompatActivity {
             }
         });
 
-        EditText textoTasaNominal = findViewById(R.id.textoTasaNominal);
-        textoTasaNominal.addTextChangedListener(new TextWatcher() {
+        binding.textoTasaNominal.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
@@ -54,8 +56,7 @@ public class SimularPlazoFijo extends AppCompatActivity {
             }
         });
 
-        EditText textoTasaEfectiva = findViewById(R.id.textoTasaEfectiva);
-        textoTasaEfectiva.addTextChangedListener(new TextWatcher() {
+        binding.textoTasaEfectiva.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
@@ -74,12 +75,10 @@ public class SimularPlazoFijo extends AppCompatActivity {
 
 
         //Captura de cambio de valor en SeekBar
-        TextView textoSeekBar = findViewById(R.id.textoSeekBar);
-        SeekBar seekBar = findViewById(R.id.seekBar1);
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        binding.seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                textoSeekBar.setText((progress*360)/100+" dias");
+                binding.textoSeekBar.setText((progress*360)/100+" dias");
                 calcular();
             }
             @Override
@@ -88,18 +87,15 @@ public class SimularPlazoFijo extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) { }
         });
 
-        Button botonConfirmar = findViewById(R.id.botonConfirmar);
-        botonConfirmar.setOnClickListener(new View.OnClickListener() {
+        binding.botonConfirmar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText textoCapital = findViewById(R.id.textoCapital);
-                SeekBar seekBar = findViewById(R.id.seekBar1);
                 Intent intentResultado = new Intent();
                 intentResultado.putExtra("capital",
                         Double.valueOf(
-                                textoCapital.getText().toString())
+                                binding.textoCapital.getText().toString())
                 );
-                intentResultado.putExtra("dias",(seekBar.getProgress()*360)/100);
+                intentResultado.putExtra("dias",(binding.seekBar1.getProgress()*360)/100);
                 setResult(Activity.RESULT_OK,intentResultado);
                 finish();
 
@@ -109,30 +105,21 @@ public class SimularPlazoFijo extends AppCompatActivity {
 
     protected void calcular(){
         //Getting Detalles
-        SeekBar seekBar = findViewById(R.id.seekBar1);
-        TextView plazo = findViewById(R.id.plazo);
-        plazo.setText("Plazo: "+(seekBar.getProgress()*360)/100+" dias");
+        binding.plazo.setText("Plazo: "+(binding.seekBar1.getProgress()*360)/100+" dias");
 
-        EditText textoCapital = findViewById(R.id.textoCapital);
-        TextView capital = findViewById(R.id.capital);
-        capital.setText("Capital: "+textoCapital.getText());
+        binding.capital.setText("Capital: "+binding.textoCapital.getText());
 
-        EditText textoTasaNominal = findViewById(R.id.textoTasaNominal);
-        EditText textoTasaEfectiva = findViewById(R.id.textoTasaEfectiva);
+//        EditText textoTasaNominal = findViewById(R.id.textoTasaNominal);
+//        EditText textoTasaEfectiva = findViewById(R.id.textoTasaEfectiva);
         //TODO calculate intereses
-        TextView interesesGanados = findViewById(R.id.interesesGanados);
-        interesesGanados.setText("Intereses ganados: $ intereses");
+        binding.interesesGanados.setText("Intereses ganados: $ intereses");
 
-        TextView montoTotal = findViewById(R.id.montoTotal);
-        montoTotal.setText("Montot total: $ Intereses + capital");
+        binding.montoTotal.setText("Montot total: $ Intereses + capital");
 
-        TextView montoTotalAnual = findViewById(R.id.montoTotalAnual);
-        montoTotalAnual.setText("Monto total anual: $"+textoTasaNominal.getText());
-
-        Button botonConfirmar = findViewById(R.id.botonConfirmar);
+        binding.montoTotalAnual.setText("Monto total anual: $"+binding.textoTasaNominal.getText());
 
         //TODO check that all the values are valid if something is invalid disable button
-        botonConfirmar.setEnabled(true);
+        binding.botonConfirmar.setEnabled(true);
     }
 
 
